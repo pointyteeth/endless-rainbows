f4e854b71a73b3567c9fd3bfc4b4fdb4364eac47
pointyteeth <xxxmeenaxxx@gmail.com>
2015-01-26 21:10:34 -0800

    triple jumping now works

--- endless rainbows/Assets/PlayerControl.cs
+++ endless rainbows/Assets/PlayerControl.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour {
 
     public float speed;
-    public float jumpSpeed;
+    public float[] jumpSpeed = new float[3];
     
     int jumpCounter = 0;
 
@@ -19,8 +19,9 @@ public class PlayerControl : MonoBehaviour {
         transform.Translate(speed*Time.deltaTime, 0f, 0f);
         
         // Jumping
-        if(Input.GetButtonDown("Jump")){;
-            rigidbody.AddForce(Vector3.up*jumpSpeed*(1f/jumpCounter));
+        if(Input.GetButtonDown("Jump") && jumpCounter < 3) {
+            rigidbody.AddForce(Vector3.up*jumpSpeed[jumpCounter]);
+            jumpCounter++;
 		}
 	}
     
