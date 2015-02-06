ad2b43470075587df2b7afbd6886eac0ea4790d8
pointyteeth <xxxmeenaxxx@gmail.com>
2015-02-05 17:51:32 -0800

    implemented basic as psl chaining system
    also made grass fill tile

--- endless rainbows/Assets/TileManager.cs
+++ endless rainbows/Assets/TileManager.cs
@@ -4,7 +4,13 @@ using System.Collections.Generic;
 
 public class TileManager : MonoBehaviour {
 
-    public Sprite[] flatTiles;
+
+    public Sprite[] fill;
+    public Sprite[] flat;
+    public Sprite[] diagonalDown;
+    public Sprite[] diagonalDownCorner;
+    
+    private Dictionary<string, SortedDictionary<double, Sprite[]>> afterDistributions;
     
     public Vector3 startPosition;
     public int numberOfTiles;
@@ -13,10 +19,21 @@ public class TileManager : MonoBehaviour {
     
 	// Use this for initialization
 	void Start() {
+        afterDistributions = new Dictionary<string, SortedDictionary<double, Sprite[]>> {
+            {"flat", new SortedDictionary<double, Sprite[]> {
+                {0.8, flat},
+                {0.2, diagonalDown}
+            }},
+            {"diagonal_down", new SortedDictionary<double, Sprite[]> {
+                {0.8, flat},
+                {0.2, diagonalDown}
+            }}
+        };
+    
         tiles = new Queue<GameObject>();
         nextPosition = startPosition;
         for(int i = 0; i < numberOfTiles; i++) {
-            tiles.Enqueue(AddTile());
+            tiles.Enqueue(InstantiateTile(flat[0]));
 		}
 	}
 	
@@ -25,19 +42,42 @@ public class TileManager : MonoBehaviour {
         if(!tiles.Peek().renderer.isVisible) {
 			GameObject old = tiles.Dequeue();
 			Object.Destroy(old);
-            tiles.Enqueue(AddTile());
+            tiles.Enqueue(InstantiateTile(GetNextTile()));
 		}
 	}
     
     // Add a tile to the foreground
-    GameObject AddTile() {
-        GameObject tile = new GameObject("tile");
+    GameObject InstantiateTile(Sprite sprite) {
+        GameObject tile = new GameObject(CleanSpriteName(sprite.ToString()));
         SpriteRenderer renderer = tile.AddComponent<SpriteRenderer>();
-        renderer.sprite = flatTiles[0];
+        renderer.sprite = sprite;
         BoxCollider2D collider = tile.AddComponent<BoxCollider2D>();
         tile.transform.position = nextPosition;
         nextPosition.x += tile.transform.localScale.x*2;
-        Debug.Log(tile.transform.localScale.x);
         return tile;
     }
+    
+    // Remove terrain type and version number from sprite name
+    string CleanSpriteName(string name) {
+        name = name.Substring(name.IndexOf("_") + 1);
+        return name.Substring(0, name.LastIndexOf("_"));
+    }
+    
+    Sprite GetNextTile() {
+        return TileFromDistribution(afterDistributions[tiles.Peek().name]);
+    }
+    
+    Sprite TileFromDistribution(SortedDictionary<double, Sprite[]> distribution) {
+        double picker = Random.value;
+        double sum = 0;
+        foreach(KeyValuePair<double, Sprite[]> pair in distribution) {
+            sum += pair.Key;
+            if(picker < sum) {
+                return pair.Value[Random.Range(0, pair.Value.Length)];
+            }
+        }
+        Debug.Log("oops");
+        return flat[0];
+    }
+    
 }
